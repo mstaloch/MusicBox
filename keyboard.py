9061913bba8c8d6873baf9b0a2863d316b2d2f5a
mstaloch <mstaloch@luc.edu>
2020-11-18 18:29:54 +0000

    added

---
+++ keyboard.py
@@ -0,0 +1,18 @@
+import pygame, time
+from pygame import mixer
+
+pygame.init()
+mixer.init()
+
+blop = pygame.mixer.Sound("/home/rambler/264/MusicBox/sounds/Blop.ogg")
+blop.play()
+
+time.sleep(3)
+
+#airhorn = pygame.mixer.Sound("home/rambler/264/MusicBox/sounds/AlienDrum.wav")
+#airhorn.play()
+
+#time.sleep(3)
+
+print("DONE")
+
