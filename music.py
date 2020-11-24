f57a9790381819849060c58640649ef83f058dd0
mstaloch <mstaloch@luc.edu>
2020-11-24 19:24:39 +0000

    moo sound added

--- music.py
+++ music.py
@@ -11,7 +11,7 @@ clang = pygame.mixer.Sound("/home/rambler/264/MusicBox/sounds/Clang.ogg")
 ting = pygame.mixer.Sound("/home/rambler/264/MusicBox/sounds/Ting.ogg")
 banner = pygame.mixer.Sound("/home/rambler/264/MusicBox/sounds/Banner.ogg")
 pling = pygame.mixer.Sound("/home/rambler/264/MusicBox/sounds/Pling.ogg")
-
+moo = pygame.mixer.Sound("/home/rambler/264/MusicBox/sounds/moo.ogg")
 
 #MAIN
 print("Press a key to play a sound! (ESC to quit)")
@@ -32,5 +32,6 @@ while True:
 				banner.play()
 			if event.key == pygame.K_t:
 				pling.play()
-
+			if event.key == pygame.K_y:
+				moo.play()
 
